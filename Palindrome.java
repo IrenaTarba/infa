0241084765bb91a864c3b92d963c7c2785de08ad
IrenaTarba <36513990+IrenaTarba@users.noreply.github.com>
2018-04-13 11:36:22 +0300

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Введите строку: ");
+
+		
+
+		String str = scan.next();
+
+
+
+		if(reverseString(str))
+
+			System.out.print("Является палиндромом!");
+
+		else
+
+			System.out.print("Не является палиндромом!");
+
+	}
+
+	
+
+	public static boolean reverseString(String s) {
+
+
+
+		for (int i = 0; i < s.length(); i++)
+
+			if(s.charAt(i) != s.charAt(s.length()-i-1))
+
+				return false;
+
+			return true;
+	}
+
+}
