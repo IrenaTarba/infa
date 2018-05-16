f8c3214c07712784f5f8124ea170b87466d70f31
IrenaTarba <36513990+IrenaTarba@users.noreply.github.com>
2018-05-16 06:20:51 +0300

    Create Point2d.java

---
+++ Point2d.java
@@ -0,0 +1,44 @@
+package point2d.java;
+import java.util.Scanner;
+public class point2d {
+	private int x;
+	private int y;
+	
+	point2d (int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+     point2d()
+     {
+    	 this.x=0;
+    	 this.y=0;
+     }
+     void SetX(int x)
+     {
+    	 this.x=x;
+     }
+     void SetY(int y)
+     {
+    	 this.y=y;
+     }
+     
+     int GetX()
+     {
+    	 return x;
+     }
+     int GetY()
+     {
+    	 return y;
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			point2d first=new point2d();
+			
+			System.out.print("x= " + first.GetX() + '\n');
+		    System.out.print("y= " + first.GetY() + '\n');
+		}
+	}
+
+}
